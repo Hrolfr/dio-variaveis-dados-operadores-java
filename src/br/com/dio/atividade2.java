21b9d56a4bae72e98cacab8deaa41458e8c71eb4
hrolfr <sabrina.sales163@gmail.com>
2022-07-15 20:10:25 -0300

    atividade 2 - fim

--- src/br/com/dio/atividade2.java
+++ src/br/com/dio/atividade2.java
@@ -9,17 +9,36 @@ public class atividade2 {
         short S = 13000;
         int i = 300;
         int I = 600;
-        long l = 3900;
-        long L = 4100;
-        float f = 2100;
-        float F = 3000;
+        long l = 39000000000L;
+        long L = 40000000000L; //quando fica muito grande, devo colocar o L para ele entender que é long
+        float f = 21.00F;
+        float F = 3.0F;
         double d = 13.500;
         double D = 900.000000;
-        char c = 't';
+        char c = 't'; //char é 1 só caractere
         char C = 'T';
         String s1 = "stonks";
         String S1 = "ratão";
         boolean b1 = false;
         boolean b2 = true;
-    }
+
+        System.out.println(b);
+        System.out.println(B);
+        System.out.println(s);
+        System.out.println(S);
+        System.out.println(i);
+        System.out.println(I);
+        System.out.println(l);
+        System.out.println(L);
+        System.out.println(f);
+        System.out.println(F);
+        System.out.println(d);
+        System.out.println(D);
+        System.out.println(c);
+        System.out.println(C);
+        System.out.println(s1);
+        System.out.println(S1);
+        System.out.println(b1);
+        System.out.println(b2);
+   }
 }
